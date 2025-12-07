6c8401f851eacd2d93e9cdd8591deda54f9376c8
Gabriel Gomes <gabrielgms0@hotmail.com>
2025-12-07 20:46:48 -0300

    feat: Adiciona Middleware Global de Erros

---
+++ MovieCatalog/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace MovieCatalog.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro inesperado capturado pelo middleware global.");
+
+                var statusCode = context.Response.StatusCode;
+
+                if (statusCode == 200)
+                {
+                    statusCode = (int)HttpStatusCode.InternalServerError;
+                }
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = statusCode;
+
+
+                var errorResponse = new
+                {
+                    message = "Ocorreu um erro inesperado no servidor. Por favor, tente novamente mais tarde.",
+                    status = statusCode,
+                    detail = ex.Message
+                };
+
+                var json = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
